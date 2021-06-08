d889ec61c7d705016d4fc9ab680b433ff767a2e5
villysiu <villysiu@gmail.com>
2021-06-08 10:48:19 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(array, func){
+    let mappedArray = [];
+    array.forEach(index => {
+        mappedArray.push(func(index))
+    });
+    return mappedArray;
+}
+
+function reduce(array, func, startingPoint){
+    let accumulator;
+    let index;
+
+    if(!!startingPoint){
+        accumulator = startingPoint;
+        index = 0;
+    } else {
+        accumulator = func(array[0],array[1]);
+        index = 2;
+    }
+    for(index; index< array.length; index++){
+        accumulator = func(accumulator, array[index])
+    }
+    return accumulator;
+}
